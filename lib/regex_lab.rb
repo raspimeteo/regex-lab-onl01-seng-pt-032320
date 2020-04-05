a3bb8e7bb389759def353129c2fde99bff8e91c3
John Zonneveld <jjczon@gmail.com>
2020-04-05 18:52:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ end
 
 def valid_phone_number?(phone)
  ## binding.pry
-  if phone == /\(?([1-9][0-9]{2})\)?[-. ]?([1-9][0-9]{2})[-. ]?([0-9]{4}\b)/
+  if !(phone =~ /\(?([1-9][0-9]{2})\)?[-. ]?([1-9][0-9]{2})[-. ]?([0-9]{4}\b)/).nil?
     true
   else
     false
